dddfa52e1600a63216e96252358def21311f5b59
KartikAiyer <kartik.aiyer@gmail.com>
2014-08-30 09:30:01 -0700

    Brought in Message Thread. Stuff Compiles, but doesn't link yet obviously. Created an API for a thread safe message queue

---
+++ MessageThread.c
@@ -0,0 +1,193 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) <2014> <Kartik Aiyer>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#include "MessageThread.h"
+#include <assert.h>
+#include "Logable.h"
+#include "Pool.h"
+#include "MessageQueue.h"
+#include "ThreadInterface.h"
+#include "SemaphoreInterface.h"
+
+typedef struct _MessageThread
+{
+  const char *threadName;
+  KThreadHandle threadHandle;
+  void* pPrivateData;
+  bool keepRunning;
+  MessageThreadInit fnInit;
+  MessageThreadProcess fnProcess;
+  MemPool pool;
+  MessageQueue messageQ;
+  uint32_t messageSize;
+  KSemaHandle semaId; 
+}MessageThread;
+
+typedef struct _MessageThreadPool
+{
+  MessageThread threads[ MESSAGE_THREADS_MAX ];
+  MemPool threadPool;
+  Logable log;
+}MessageThreadPool;
+
+static MessageThreadPool s_threadPool = { .log = { .prefix = "MsgThrPool", .enabled = true } };
+
+#define MSG_POOL_LOG( str, ... )      Log( &s_threadPool.log, str, ##__VA_ARGS__ )
+
+static void Thread( void *arg );
+
+void MessageThreadSystemInit( void )
+{
+  if ( !PoolCreate( &s_threadPool.threadPool, (uint8_t* )s_threadPool.threads, sizeof(s_threadPool.threads ), MESSAGE_THREADS_MAX ) ) {
+    assert( 0 );
+  }
+}
+
+MessageThreadHandle MessageThreadCreate( const MessageThreadDef *pThreadParams )
+{
+  assert( pThreadParams );
+  MessageThreadHandle retval = NULL;
+  MessageThread *pThread = NULL;
+
+  //TODO: These two lines should be atomic
+  pThread = ( MessageThread* )PoolAlloc( &s_threadPool.threadPool );
+  if ( pThread ) {
+
+    pThread->semaId = KSemaCreate( pThreadParams->threadName, 0 );
+    if ( pThread->semaId ) {
+      pThread->threadName = pThreadParams->threadName;
+      pThread->messageSize = pThreadParams->messageSize;
+      pThread->fnInit = pThreadParams->fnInit;
+      pThread->fnProcess = pThreadParams->fnProcess;
+      pThread->pPrivateData = pThreadParams->pPrivateData;
+      pThread->keepRunning = true;
+      assert( pThread->fnInit && pThread->fnProcess );
+
+      if( MessageQueueInitialize( &pThread->messageQ ) )
+      {
+        if( PoolCreate( &pThread->pool, 
+                    pThreadParams->messageBackingStore, 
+                    pThreadParams->messageSize * pThreadParams->messageQDepth, 
+                    pThreadParams->messageQDepth ) ) {
+
+          pThread->threadHandle = KThreadCreate( Thread, pThread, pThreadParams->stackSize, pThreadParams->priority );
+          if ( pThread->threadHandle ) {
+            //Will block till initialization of thread is complete. 
+            KSemaGet( pThread->semaId, WAIT_FOREVER );
+            KSemaDelete( pThread->semaId );
+            pThread->semaId = 0;
+            retval = ( MessageThreadHandle )pThread;
+          }
+          else {
+            MSG_POOL_LOG( "%s(): Couldn't create Thread", __FUNCTION__ );
+            PoolRelease( &pThread->pool );
+            MessageQueueDeInitialize( &pThread->messageQ );
+            KSemaDelete( pThread->semaId );
+            PoolFree( &s_threadPool.threadPool, pThread );
+          }
+        }
+        else {
+          MSG_POOL_LOG( "%s(): Cannot Create Pool", __FUNCTION__ );
+          MessageQueueDeInitialize( &pThread->messageQ );
+          KSemaDelete( pThread->semaId );
+          PoolFree( &s_threadPool.threadPool, pThread );
+        }
+      }
+      else {
+        MSG_POOL_LOG( "%s(): Cannot Create Message Queue", __FUNCTION__ );
+        KSemaDelete( pThread->semaId );
+        PoolFree( &s_threadPool.threadPool, pThread );
+      }
+    }
+    else {
+      MSG_POOL_LOG( "%s(): Couldn't Create Thread Semaphore", __FUNCTION__ );
+      PoolFree( &s_threadPool.threadPool, pThread );
+    }
+  }
+  else {
+    MSG_POOL_LOG( "%s(): Reached Message Thread Maximum count", __FUNCTION__ );
+  }
+  return retval;
+}
+
+void* MessageThreadGetPrivateData( MessageThreadHandle hThread ) 
+{
+  MessageThread *pThread = ( MessageThread * )hThread;
+  return pThread->pPrivateData;
+}
+
+MessageHandle MessageThreadAllocateMessage( MessageThreadHandle hThread )
+{
+  MessageThread *pThread = ( MessageThread * )hThread;
+  MessageHandle retval = NULL;
+  retval = ( MessageHandle )PoolAlloc( &pThread->pool );
+  if( !retval  ) {
+    MSG_POOL_LOG( "%s: Couldn't allocate message", __FUNCTION__ );
+    assert( 0 );
+  }
+  return retval;
+}
+
+void MessageThreadDestroyMessage( MessageThreadHandle hThread, MessageHandle* phMessage )
+{
+  MessageThread *pThread = ( MessageThread* )hThread;
+  if( phMessage && *phMessage ) {
+    PoolFree( &pThread->pool, *phMessage );
+    *phMessage = NULL;
+  }
+}
+
+bool MessageThreadPost( MessageThreadHandle hThread, MessageHandle hMessage )
+{
+  bool retval = true;
+  MessageThread *pThread = ( MessageThread* )hThread;
+  if ( !MessageQueueEnQueue( &pThread->messageQ, hMessage ) ) {
+    MSG_POOL_LOG( "Couldn't post message onto Q." );
+    //For now these calls shouldn't fail
+    assert( 0 );
+    retval = false;
+  }
+  return retval;
+}
+
+static void Thread( void *arg )
+{
+  MessageThread *pThread = ( MessageThread* )arg;
+  assert( pThread );
+  //Call private Init
+  pThread->fnInit( arg );
+  assert( pThread->semaId );
+  KSemaPut( pThread->semaId );
+  while( pThread->keepRunning ) {
+    KListElem* pMsg = MessageQueueDeQueue( &pThread->messageQ );
+    if( pMsg ){
+      pThread->fnProcess( arg, pMsg );
+      //Delete the message
+      MessageThreadDestroyMessage( arg, &pMsg );
+    }
+    else{
+      MSG_POOL_LOG( "Couldn't pull message of Q" );
+      assert( 0 );
+    }
+  }
+}
---
+++ MessageThread.h
@@ -0,0 +1,165 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) <2014> <Kartik Aiyer>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#ifndef __MESSAGE_THREAD_H__
+#define __MESSAGE_THREAD_H__
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "klist.h"
+
+
+#define MESSAGE_THREADS_MAX            ( 5 )
+
+typedef KListElem* MessageHandle;
+typedef const void* MessageThreadHandle;
+
+/**
+ * The function callback that is called to initialize the thraed 
+ * when the thread is started. MessageThreadCreate will not 
+ * return till this function is completed. 
+ *  
+ * @param MessageThreadHandle - Handle to the message thread. 
+ */
+typedef void (*MessageThreadInit)( MessageThreadHandle hThread );
+
+/**
+ * This callback is called when a message is received in the 
+ * internal message queue. 
+ *  
+ * @param hThread - Handle to the mesage thread. 
+ * @param hMessage - Handle to the message recd. 
+ */
+typedef void (*MessageThreadProcess)( MessageThreadHandle hThread, MessageHandle hMessage );
+
+/**
+ * @struct MessageThreadDef 
+ * @brief - Message thread initialization structure 
+ *  
+ * Need to specify, stack size, priority, number of entries that 
+ * can be held in the message Q, size of the messages, size of 
+ * the pool from which messages are allocated and any private 
+ * data. 
+ */
+typedef struct _MessageThreadDef
+{
+  const char* threadName;   /**< Name of thread. A statically allocated name is best. */
+  uint32_t stackSize;       /**< Stack size in bytes */
+  uint32_t priority;
+  uint8_t* messageBackingStore;
+  uint32_t messageQDepth;   /**< The number of messages that can Q up in the thread  */
+  uint32_t messageSize;     /**< The size of each message processed by the thread */
+  void *pPrivateData;       /**< Private data that is passed to the thread functions. Holds thread state. */
+  MessageThreadInit fnInit; /**< Thread Initialization function */
+  MessageThreadProcess fnProcess; /**< Function to process each incoming message */
+}MessageThreadDef;
+
+/**
+ * A Convenience method used to create a MessageThreadDef to 
+ * be used with message Thread Create. 
+ */
+#define MESSAGE_THREAD_DEF( name, stkSz, pri, msgBackStore, qDepth, msgType, msgCount, priv, init, process )\
+const MessageThreadDef messageThreadDef_##name =\
+{\
+  .threadName = #name,\
+  .stackSize = stkSz,\
+  .priority = pri,\
+  .messageBackingStore = msgBackStore,\
+  .messageQDepth = qDepth,\
+  .messageSize = sizeof(msgType),\
+  .messagePoolCount = msgCount,\
+  .pPrivateData = priv,\
+  .fnInit = init,\
+  .fnProcess = process\
+}
+
+#define MESSAGE_THREAD( name ) &messageThreadDef_##name
+
+/**
+ * Used to create a message thread. It is not safe to post 
+ * events to the message thread till this function returns. The 
+ * function will not return till the MessageThreadInit function 
+ * supplied by the client instance completes.  
+ * 
+ * 
+ * @param pThreadDef: const MessageThreadDef* - Message thread 
+ *                  initialization params.
+ * 
+ * @return MessageThreadHandle - Handle to the message thread.
+ */
+MessageThreadHandle MessageThreadCreate( const MessageThreadDef *pThreadDef );
+
+/**
+ * Used to get a pointer to the private data that was supplied 
+ * to the message thread during creation. 
+ * 
+ * 
+ * @param hThread: MessageThreadHandle - Handle to message 
+ *               thread.
+ * 
+ * @return void* - pointer to private data if any. 
+ */
+void* MessageThreadGetPrivateData( MessageThreadHandle hThread );
+
+/**
+ * Allcoate a message that will be used to post to the message 
+ * thread. Cliednt should ideally make convenience routines that 
+ * combine MessageThreadAllocateMessage() and 
+ * MessageThreadPost(). 
+ * 
+ * 
+ * @param hThread: MessageThreadHandle - Handle to message 
+ *               thread.
+ * 
+ * @return MessageHandle - Handle to message
+ */
+MessageHandle MessageThreadAllocateMessage( MessageThreadHandle hThread );
+
+/**
+ * Used to destroy a message. External clients don't have to 
+ * call this function if the messsage is posted to the thread. 
+ * The thread will automatically delete the message after the 
+ * Process Callback is called on the message. 
+ * 
+ * 
+ * @param hThread: MessageThreadHandle - handle to thread
+ * @param phMessage: MessageHandle* - pointer to messsage 
+ *                 handle.
+ */
+void MessageThreadDestroyMessage( MessageThreadHandle hThread, MessageHandle* phMessage );
+
+/**
+ * Used to post messages to the message thread. Clients should 
+ * combine this with MessageThreadAllocate to make a convenience 
+ * routine to post events to their threads. 
+ * 
+ * 
+ * @param hThread: MessageThreadHandle - Handle to message 
+ *               thread
+ * @param hMessage: MessageHandle - Handle to message
+ * 
+ * @return bool - True if the event has been posted.
+ */
+bool MessageThreadPost( MessageThreadHandle hThread, MessageHandle hMessage );
+
+#endif // __MESSAGE_THREAD_H__
