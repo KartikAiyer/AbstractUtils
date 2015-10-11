d8d6055f0c9b5f5e115845cf7e3f90b29467135b
Kartik Aiyer <kartik@jauntvr.com>
2015-10-11 14:45:04 -0700

    Moved some tests into new folders and added Embedded Unit

---
+++ Tests/embtests/AllTests.c
@@ -0,0 +1,8 @@
+#include<embUnit.h>
+#include <ThreadInterface.h>
+
+int main (int argc, const char* argv[])
+{
+
+}
+
---
+++ Tests/embtests/PoolTest.c
@@ -0,0 +1,21 @@
+#include <embUnit/embUnit.h>
+#include <Pool.h>
+
+
+typedef struct _PoolTestBasicData
+{
+  uint8_t poolStoreSize;
+}PoolTestBasicData;
+
+static void setUp()
+{
+
+}
+static void tearDown()
+{
+
+}
+
+TestRef PoolTest_tests(void)
+{
+}
